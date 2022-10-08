112bc397131336752e15866b4b36a1656c7e2b87
MartinMesko <alegremato@gmail.com>
2022-10-08 21:34:04 +0200

    refactoring

--- src/formatCSV/Uzivatel.java
+++ src/formatCSV/Uzivatel.java
@@ -12,4 +12,23 @@ public class Uzivatel {
         this.vek = vek;
         this.registrovany = registrovany;
     }
+
+    public String getMeno() {
+        return meno;
+    }
+
+    public int getVek() {
+        return vek;
+    }
+
+    public LocalDate getRegistrovany() {
+        return registrovany;
+    }
+
+    @Override
+    public String toString() {
+        return meno;
+    }
+
+
 }
